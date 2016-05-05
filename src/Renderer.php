30cd4d2c44a1abe3d83d1cea39a91017a8e4031b
Assouan Dinari <git@aceougi.com>
2016-05-05 08:50:49 +0200

    Updated Renderer class for can be instantiated

--- src/Renderer.php
+++ src/Renderer.php
@@ -4,25 +4,117 @@ namespace AceOugi;
 
 class Renderer
 {
+    /** @var array */
+    protected static $__data = [];
+
+    /** @var string */
+    protected $path;
+    /** @var array */
+    protected $data;
+
     /**
-     * @param string $__PATH
-     * @param array $__DATA
+     * Renderer constructor.
+     * @param string $path
+     * @param array $data
      */
-    public static function display(string $__PATH, array $__DATA = [])
+    public function __construct(string $path, array $data = [])
     {
-        extract($__DATA);
-        include $__PATH;
+        $this->path = $path;
+        $this->data = $data;
     }
 
     /**
-     * @param string $__PATH
-     * @param array $__DATA
+     * @param string $key
+     * @param $value
+     */
+    public static function set(string $key, $value)
+    {
+        static::$__data[$key] = $value;
+    }
+
+    /**
+     * Alias of ::set
+     * @param string $key
+     * @param $value
+     */
+    public static function share(string $key, $value)
+    {
+        static::set($key, $value);
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     */
+    public static function get(string $key)
+    {
+        return static::$__data[$key] ?? null;
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public static function has(string $key)
+    {
+        return (isset(static::$__data[$key]) OR array_key_exists($key, static::$__data)) ? true : false;
+    }
+
+    /**
+     * @param string $key
+     * @param $value
+     * @return self
+     */
+    public function with(string $key, $value)
+    {
+        $new = clone $this;
+        $new->data[$key] = $value;
+        return $new;
+    }
+
+    /**
+     * @param string $key
+     * @return self
+     */
+    public function without(string $key)
+    {
+        $new = clone $this;
+        unset($new->data[$key]);
+        return $new;
+    }
+
+    public function make()
+    {
+        extract(static::$__data + $this->data);
+        include $this->path;
+    }
+
+    /**
      * @return string
      */
-    public static function render(string $__PATH, array $__DATA = [])
+    public function makeToString()
     {
         ob_start();
-        static::display($__PATH, $__DATA);
+        $this->make();
         return ob_get_clean();
+    }
+
+    /**
+     * @param string $path
+     * @param array $data
+     */
+    public static function display(string $path, array $data = [])
+    {
+        (new static($path, $data))->make();
+    }
+
+    /**
+     * @param string $path
+     * @param array $data
+     * @return string
+     */
+    public static function render(string $path, array $data = [])
+    {
+        return (new static($path, $data))->makeToString();
     }
 }
